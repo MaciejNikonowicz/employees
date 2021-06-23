6f1655094b25b3840739d062f7f1d3d8751a02b0
Maciej Nikonowicz <maciejn@mikronika.com.pl>
2021-06-23 13:39:43 +0200

    Countries Update/Delete

--- app/Http/Controllers/Backend/CountryController.php
+++ app/Http/Controllers/Backend/CountryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CountryStoreRequest;
+use App\Http\Requests\CountryUpdateRequest;
 use App\Models\Country;
 use Illuminate\Http\Request;
 
@@ -32,6 +33,22 @@ class CountryController extends Controller
 
     public function edit(Country $country)
     {
-        return view('countries.edit');
+        return view('countries.edit', compact('country'));
+    }
+
+    public function update(CountryUpdateRequest $request, Country $country)
+    {
+        $country->update([
+            'country_code' => $request->country_code,
+            'name' => $request->name
+        ]);
+
+        return redirect()->route('countries.index')->with('message', 'Country has been updated successfully');
+    }
+
+    public function destroy(Country $country)
+    {
+        $country->delete();
+        return redirect()->route('countries.index')->with('message', 'Country has been deleted successfully');
     }
 }
--- resources/views/countries/edit.blade.php
+++ resources/views/countries/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Users</h1>
+        <h1 class="h3 mb-0 text-gray-800">Country: {{ $country->name }}</h1>
     </div>
 
     <div class="container">
@@ -11,20 +11,20 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        {{ __('Update User') }}
-                        <a href="{{ route('users.index') }}" class="float-right btn btn-danger">Back</a>
+                        {{ __('Update Country') }}
+                        <a href="{{ route('countries.index') }}" class="float-right btn btn-danger">Back</a>
                     </div>
                     <div class="card-body">
-                        <form method="POST" action="{{ route('users.update', $user->id) }}">
+                        <form method="POST" action="{{ route('countries.update', $country->id) }}">
                             @csrf
                             @method('PUT')
                             <div class="form-group row">
-                                <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                                <label for="country_code" class="col-md-4 col-form-label text-md-right">{{ __('Country Code') }}</label>
     
                                 <div class="col-md-6">
-                                    <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username', $user->username) }}" required autocomplete="username" autofocus>
+                                    <input id="country_code" type="text" class="form-control @error('country_code') is-invalid @enderror" name="country_code" value="{{ old('country_code', $country->country_code) }}" required autocomplete="country_code" autofocus>
     
-                                    @error('username')
+                                    @error('country_code')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
@@ -33,40 +33,12 @@
                             </div>
     
                             <div class="form-group row">
-                                <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('First Name') }}</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
     
                                 <div class="col-md-6">
-                                    <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name', $user->first_name) }}" required autocomplete="name" autofocus>
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name', $country->name) }}" required autocomplete="name" autofocus>
     
-                                    @error('first_name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-    
-                            <div class="form-group row">
-                                <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name', $user->last_name) }}" required autocomplete="name" autofocus>
-    
-                                    @error('last_name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-    
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', $user->email) }}" required autocomplete="email">
-    
-                                    @error('email')
+                                    @error('name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
@@ -85,52 +57,11 @@
                     </div>
                 </div>
                 <div class="m-2 p-2">
-                    <form method="POST" action="{{ route('users.destroy', $user->id) }}">
+                    <form method="POST" action="{{ route('countries.destroy', $country->id) }}">
                         @csrf
                         @method('DELETE')
-                        <button class="btn btn-danger">Delete {{ $user->username }}</button>
+                        <button class="btn btn-danger">Delete {{ $country->country_code }}</button>
                     </form>
-                </div>
-            </div>
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Change Password') }}</div>
-    
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('users.change.password', $user->id) }}">
-                            @csrf
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-    
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-    
-                            <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                </div>
-                            </div>
-    
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Update Password') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
                 </div>
             </div>
         </div>
