9024593c6d5e7416a5c016796c5f21468c7a575a
Maciej Nikonowicz <maciejn@mikronika.com.pl>
2021-06-25 13:14:10 +0200

    First Vue element - List Employees + create form

---
+++ resources/js/components/employees/Index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Employees</h1>
+    </div>
+
+    <div class="row">
+        <div class="card mx-auto">
+            <div>
+                <div class="alert alert-success"></div>
+            </div>
+            <div class="card-header">
+                <div class="row">
+                    <div class="col">
+                        <form method="GET" action="">
+                            <div class="form-row align-items-center">
+                                <div class="col">
+                                    <input type="search" name="search" id="inlineFormInput" class="form-control mb-2" placeholder="Search...">
+                                </div>
+                                <div class="col">
+                                    <button type="submit" class="btn btn-primary mb-2">Search</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <router-link :to="{name: 'EmployeesCreate'}" class="float-right btn btn-secondary mb-2">Add</router-link>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#ID</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Department</th>
+                        <th scope="col">Manage</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <a href="" class="btn btn-success">Edit</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                  </table>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
